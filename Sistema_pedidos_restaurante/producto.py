aac63db8fe790d21da929124954bd1765c8a9fe3
franespinola <r.espinola@alumno.um.edu.ar>
2024-04-29 10:57:45 -0300

    Refactor cocina.py, producto.py, menus.py, and servidor.py files

--- Sistema_pedidos_restaurante/producto.py
+++ Sistema_pedidos_restaurante/producto.py
@@ -7,26 +7,4 @@ class Producto:
         self.descripcion = descripcion
 
     def __str__(self):
-        return f"""
-        Producto: {self.nombre}
-        ID: {self.id}
-        Precio: ${self.precio:.2f}
-        Categoría: {self.categoria}
-        Descripción: {self.descripcion}
-        """
-
-class PedidoItem:
-    def __init__(self, producto, cantidad, observaciones):
-        self.producto = producto
-        self.cantidad = cantidad
-        self.observaciones = observaciones
-
-    def calcular_subtotal(self):
-        return self.producto.precio * self.cantidad
-
-    def __str__(self):
-        return f"""
-        Item: {self.producto.nombre} ({self.cantidad} unidades)
-        Subtotal: ${self.calcular_subtotal():.2f}
-        Observaciones: {self.observaciones}
-        """
+        return f"{self.nombre} ({self.categoria}): {self.descripcion} - ${self.precio:.2f}"
--- Sistema_pedidos_restaurante/servidor.py
+++ Sistema_pedidos_restaurante/servidor.py
@@ -1,40 +1,7 @@
-import socket
-import threading
-from restaurante import Restaurante
-from menus import carta
 
-restaurante = Restaurante(carta) #tengo que pasar la carta como parametro
 
-def handle_client(client_socket):
-    while True:
-        # Recibir la elección del cliente
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        opcion = data.decode()
-        if opcion == "1":
-            respuesta = restaurante.mostrar_carta()
-        elif opcion == "2":
-            respuesta = restaurante.tomar_pedido()
-        elif opcion == "3":
-            respuesta = restaurante.mostrar_pedidos()
-        elif opcion == "4":
-            respuesta = "Saliendo del programa."
-        else:
-            respuesta = "Opción no válida."
-        client_socket.sendall(respuesta.encode())
 
-def server_loop():
-    HOST = 'localhost'
-    PORT = 50007
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(5)
-    while True:
-            conn, addr = s.accept()
-            client_thread = threading.Thread(target=handle_client, args=(conn,))
-            client_thread.start()
-server_loop()
+
 
 
     
